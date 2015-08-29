c72a236f6d37d9f72caf0533101816619d4c89a0
Karen Etheridge <ether@cpan.org>
2015-08-28 22:02:59 -0700

    fix vim modeline

--- lib/Dist/Zilla/Plugin/Chrome/ExtraPrompt.pm
+++ lib/Dist/Zilla/Plugin/Chrome/ExtraPrompt.pm
@@ -3,7 +3,7 @@ use warnings;
 package Dist::Zilla::Plugin::Chrome::ExtraPrompt;
 # ABSTRACT: Perform arbitrary commands when Dist::Zilla prompts you
 # KEYWORDS: prompt execute command external
-# vim: set ts=8 sts=4 sw=4 tw=78 et :
+# vim: set ts=8 sts=4 sw=4 tw=115 et :
 
 our $VERSION = '0.015';
 
